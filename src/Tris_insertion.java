b6afb7169ab529e86c90601c51236ad0c16b38af
d19022846 <jonathandiki@outlook.com>
2020-01-23 14:40:15 +0100

    Initial commit

---
+++ src/Tris_insertion.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+
+
+class  ApplicationTriBulle
+{
+    static  int [] table  =  new  int [10]  ;  // le tableau è trier en attribut
+
+    static void  TriBulle  ( )
+    {
+        int  n  =  table.length - 1 ;
+        for (  int  i  =  n ;  i >= 1 ;  i -- )
+            for (  int  j  =  2 ;  j <= i ;  j ++ )
+                if ( table[j - 1]  >  table[j] )
+                {
+                    int  temp  =  table[j - 1] ;
+                    table[j - 1]  =  table[j] ;
+                    table[j]  =  temp ;
+                }
+    }
+
+
+
+
+    public class Tris_insertion {
+
+        public int minimum;
+        public int maximum, i ;
+
+
+
+        Arraylist<> tab =new ArrayList<>();
+
+for (i =0, i<=12 ; i++)
+        {
+
+        }
+
+    }
