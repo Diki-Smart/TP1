17235ef05e0c406745b1cdb77c44569e4d10a726
d19022846 <jonathandiki@outlook.com>
2020-01-30 14:43:37 +0100

    new

---
+++ src/TriBulle.java
@@ -0,0 +1,18 @@
+public class TriBulle {
+
+    public static void main (String[] args)
+      {
+
+          int i;
+          int Tab[];
+
+
+          for (i=0 ; i < 10 ; i++)
+          {
+
+
+
+          }
+
+      }
+}
