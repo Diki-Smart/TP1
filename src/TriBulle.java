1f44409121c934d9f4eb449d6d21e619249c1a54
d19022846 <jonathandiki@outlook.com>
2020-02-06 16:19:35 +0100

    fusion

--- src/TriBulle.java
+++ src/TriBulle.java
@@ -1,69 +1,38 @@
-/*public class TriBulle {
+public class TriBulle {
 
-    public static void main (String[] args)
-      {
-
-          int i;
-          int Tab[];
-
-
-          for (i=0 ; i < 10 ; i++)
-          {
-
-              if ( Tab[i] < Tab[i])
-              {
-
-              }
-
-
-
-
-
-          }
-
-      }
-}
-*/
-    public static void triBulle(int tableau[])
-    {
-        int longueur=tableau.length;
+    public static void triBulle(int tableau[]) {
+        int longueur = tableau.length;
         boolean inversion;
 
-        do
-        {
-            inversion=false;
+        do {
+            inversion = false;
 
-            for(int i=0;i<longueur-1;i++)
-            {
-                if(tableau[i]>tableau[i+1])
-                {
-                    echanger(tableau,i,i+1);
-                    inversion=true;
+            for (int i = 0; i < longueur - 1; i++) {
+                if (tableau[i] > tableau[i + 1]) {
+                    echanger(tableau, i, i + 1);
+                    inversion = true;
                 }
             }
         }
-        while(inversion);
+        while (inversion);
     }
 
 
-    public static void triBulleOptimise(int tableau[])
-    {
-        int longueur=tableau.length;
+    public static void triBulleOptimise(int tableau[]) {
+        int longueur = tableau.length;
         boolean inversion;
 
-        do
-        {
-            inversion=false;
+        do {
+            inversion = false;
 
-            for(int i=0;i<longueur-1;i++)
-            {
-                if(tableau[i]>tableau[i+1])
-                {
-                    echanger(tableau,i,i+1);
-                    inversion=true;
+            for (int i = 0; i < longueur - 1; i++) {
+                if (tableau[i] > tableau[i + 1]) {
+                    echanger(tableau, i, i + 1);
+                    inversion = true;
                 }
             }
             longueur--;
         }
-        while(inversion);
+        while (inversion);
     }
+}
